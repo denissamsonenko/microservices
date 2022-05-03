08f475ee369ab9691a327a01d0ff61ec6701238b
denissamsonenko <denissamsonenko@mail.ru>
2022-05-03 18:59:02 +0300

    added service

---
+++ fraud/src/main/java/com/company/fraud/FraudApplication.java
@@ -0,0 +1,16 @@
+package com.company.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Denis Samsonenko
+ * @created : 03.05.2022
+ */
+
+@SpringBootApplication
+public class FraudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
