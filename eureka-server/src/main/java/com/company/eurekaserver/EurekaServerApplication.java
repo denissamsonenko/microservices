426e9b11215cb92f65bc75e02c6f8c77f08df4b6
denissamsonenko <denissamsonenko@mail.ru>
2022-05-03 22:47:55 +0300

    eureka added

---
+++ eureka-server/src/main/java/com/company/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.company.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : Denis Samsonenko
+ * @created : 03.05.2022
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
--- fraud/src/main/java/com/company/fraud/controller/FraudController.java
+++ fraud/src/main/java/com/company/fraud/controller/FraudController.java
@@ -3,6 +3,7 @@ package com.company.fraud.controller;
 import com.company.fraud.dto.FraudResponse;
 import com.company.fraud.service.FraudCheckService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @created : 03.05.2022
  */
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
@@ -21,6 +23,8 @@ public class FraudController {
 
     @GetMapping(path = "{customerId}")
     public FraudResponse isFraudster(@PathVariable("customerId") Integer customerId) {
-        return new FraudResponse(fraudCheckService.isFraudulentCustomer(customerId));
+        Boolean isFraud = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check for customer {}", customerId);
+        return new FraudResponse(isFraud);
     }
 }
